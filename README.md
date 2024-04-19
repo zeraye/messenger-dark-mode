e4194e9cfa99ef4d89f4f3e1090c06e287dee6b9
zeraye <zeraye@protonmail.com>
2024-04-19 14:38:23 +0200

    chore: init

---
+++ README.md
@@ -0,0 +1,7 @@
+# messenger-dark-mode
+
+Enable dark mode for [Messenger](https://www.messenger.com/).
+
+## addon
+
+Extension can be installed [here](https://addons.mozilla.org/en-US/firefox/addon/messenger-dark-mode/).
---
+++ src/js/index.js
@@ -0,0 +1,21 @@
+/* Replace all data in `.__fb-light-mode` by data in `.__fb-light-mode`.
+ */
+(() => {
+  const fbStyle = document.head.querySelectorAll("style")[0].textContent;
+
+  // remove `.__fb-light-mode`
+  const patternLightMode = /\.__fb-light-mode\s*{[^}]*}/g;
+  const cssWithoutLightMode = fbStyle.replace(patternLightMode, "");
+
+  // add `.__fb-light-mode`, with same data as in `.__fb-dark-mode`
+  const pattern2 = /\.(__fb-dark-mode)\s*{([^}]*)}/g;
+  const cssWithModifiedLightMode = cssWithoutLightMode.replace(
+    pattern2,
+    (match, _, p2) => {
+      return match + "\n.__fb-light-mode {" + p2 + "}";
+    }
+  );
+
+  document.head.querySelectorAll("style")[0].textContent =
+    cssWithModifiedLightMode;
+})();
